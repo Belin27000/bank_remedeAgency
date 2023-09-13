631268152952dec5f4648d6ccf480635ffb0f8e8
Belin27000 <y.lecerf@protonmail.com>
2023-09-13 11:28:55 +0200

    first commit

---
+++ p13-remedeAgency_frontEnd/src/pages/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div className='test'>
+            ca marche tu crois serieux
+            ok
+        </div>
+    );
+};
+
+export default Test;
